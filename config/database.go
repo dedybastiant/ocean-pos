19abbb5c5269d43d5f0971399089e0393c5ba53e
dedybastiant <dedybastian.t@gmail.com>
2024-06-13 11:54:17 +0700

    Add database config

---
+++ config/database.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+func NewDB() *sql.DB {
+	config := viper.New()
+	config.SetConfigFile("config.env")
+	config.AddConfigPath(".")
+
+	err := config.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	username := config.GetString("DATABASE_USERNAME")
+	password := config.GetString("DATABASE_PASSWORD")
+	db_url := config.GetString("DATABASE_URL")
+	db_port := config.GetString("DATABASE_PORT")
+	db_name := config.GetString("DATABASE_NAME")
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", username, password, db_url, db_port, db_name)
+
+	db, err := sql.Open("mysql", connection)
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxIdleTime(10 * time.Second)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
