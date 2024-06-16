40fafe45351ed7f90107803b582b5daa2ffbd429
dedybastiant <dedybastian.t@gmail.com>
2024-06-16 13:01:09 +0700

    Add logout controller

--- internal/controller/auth_controller.go
+++ internal/controller/auth_controller.go
@@ -4,12 +4,14 @@ import (
 	"net/http"
 	"ocean-pos/internal/dto"
 	"ocean-pos/internal/service"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 type AuthController interface {
 	Login(c *gin.Context)
+	Logout(c *gin.Context)
 }
 
 type AuthControllerImpl struct {
@@ -23,10 +25,54 @@ func NewAuthController(service service.AuthService) AuthController {
 }
 
 func (controller *AuthControllerImpl) Login(c *gin.Context) {
-	var request dto.AuthRequest
+	var request dto.LoginRequest
 	c.BindJSON(&request)
 
 	response, err := controller.AuthService.Login(c, request)
+	if err != nil {
+		switch err.Error() {
+		case "EMAIL_NOT_FOUND":
+			c.IndentedJSON(http.StatusNotFound,
+				dto.CommonResponse{
+					Code:        http.StatusNotFound,
+					Status:      "NOT_FOUND",
+					Description: "email not registered",
+				})
+		case "INCORRECT_CREDENTIAL":
+			c.IndentedJSON(http.StatusBadRequest,
+				dto.CommonResponse{
+					Code:        http.StatusBadRequest,
+					Status:      "BAD_REQUEST",
+					Description: "incorrect password",
+				})
+		default:
+			c.IndentedJSON(http.StatusInternalServerError,
+				dto.CommonResponse{
+					Code:        http.StatusInternalServerError,
+					Status:      "INTERNAL_SERVER_ERROR",
+					Description: "something went wrong",
+				})
+		}
+	} else {
+		c.IndentedJSON(http.StatusOK,
+			dto.CommonResponse{
+				Code:   http.StatusOK,
+				Status: "SUCCESS",
+				Data:   response,
+			})
+	}
+}
+
+func (controller *AuthControllerImpl) Logout(c *gin.Context) {
+	var request dto.LogoutRequest
+	c.BindJSON(&request)
+	authHeader := c.Request.Header.Get("Authorization")
+
+	refreshToken := request.RefreshToken
+	accessToken := strings.Split(authHeader, " ")[1]
+
+	response, err := controller.AuthService.Logout(c, accessToken, refreshToken)
+
 	if err != nil {
 		switch err.Error() {
 		case "EMAIL_NOT_FOUND":
