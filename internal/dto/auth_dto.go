ffa462550a1a080d8d324dfe3c332ff3e9d7ce12
dedybastiant <dedybastian.t@gmail.com>
2024-06-16 12:49:15 +0700

    Update dto from auth to more specific (login and logout)

--- internal/dto/auth_dto.go
+++ internal/dto/auth_dto.go
@@ -4,18 +4,22 @@ import (
 	"ocean-pos/internal/model"
 )
 
-type AuthRequest struct {
+type LoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
-type AuthResponse struct {
+type LogoutRequest struct {
+	RefreshToken string `json:"refresh_token"`
+}
+
+type LoginResponse struct {
 	Token        string `json:"token"`
 	RefreshToken string `json:"refresh_token"`
 }
 
-func GenerateAuthResponse(auth model.Auth) AuthResponse {
-	return AuthResponse{
+func GenerateLoginResponse(auth model.Auth) LoginResponse {
+	return LoginResponse{
 		Token:        auth.Token,
 		RefreshToken: auth.RefreshToken,
 	}
