bfdb787b9e1742c19ced112e676e15218ca38bd5
dedybastiant <dedybastian.t@gmail.com>
2024-06-18 22:04:46 +0700

    Add database migration for business

---
+++ migrations/20240618083041_create_table_business.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE business (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    owner_user_id INT NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    verified_at TIMESTAMP DEFAULT NULL,
+    deactivated_at TIMESTAMP DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_by INT NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+    updated_by INT NOT NULL,
+    FOREIGN KEY (owner_user_id) REFERENCES user(id)
+) ENGINE = InnoDB;
