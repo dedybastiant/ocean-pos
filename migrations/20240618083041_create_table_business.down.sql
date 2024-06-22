e82dd6042c80ccfcf1166fbf97e41edb49ca555a
dedybastiant <dedybastian.t@gmail.com>
2024-06-22 11:23:42 +0700

    update migration file

--- migrations/20240618083041_create_table_business.down.sql
+++ migrations/20240618083041_create_table_business.down.sql
@@ -1,1 +1,3 @@
+ALTER TABLE business DROP FOREIGN KEY business_ibfk_1;
+
 DROP TABLE IF EXISTS business;
--- migrations/20240618083041_create_table_business.up.sql
+++ migrations/20240618083041_create_table_business.up.sql
@@ -4,11 +4,11 @@ CREATE TABLE business (
     email VARCHAR(255) NOT NULL,
     phone_number VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    verified_at TIMESTAMP DEFAULT NULL,
-    deactivated_at TIMESTAMP DEFAULT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    verified_at TIMESTAMP(6) DEFAULT NULL,
+    deactivated_at TIMESTAMP(6) DEFAULT NULL,
+    created_at TIMESTAMP(6) NOT NULL,
     created_by INT NOT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP(6) NOT NULL,
     updated_by INT NOT NULL,
     FOREIGN KEY (owner_user_id) REFERENCES user(id)
 ) ENGINE = InnoDB;
