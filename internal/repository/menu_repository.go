539f4b7825eee1ca9913b72889bbbf4efeb558d4
dedybastiant <dedybastian.t@gmail.com>
2024-06-24 08:02:39 +0700

    Add Function FindMenuById

--- internal/repository/menu_repository.go
+++ internal/repository/menu_repository.go
@@ -10,6 +10,7 @@ import (
 type MenuRepository interface {
 	InsertMenu(ctx context.Context, tx *sql.Tx, menu model.Menu) (*model.Menu, error)
 	FindMenuByName(ctx context.Context, tx *sql.Tx, menuName string) (*model.Menu, error)
+	FindMenuById(ctx context.Context, tx *sql.Tx, menuId int) (*model.Menu, error)
 }
 
 type MenuRepositoryImpl struct{}
@@ -35,13 +36,39 @@ func (repository *MenuRepositoryImpl) InsertMenu(ctx context.Context, tx *sql.Tx
 }
 
 func (repository *MenuRepositoryImpl) FindMenuByName(ctx context.Context, tx *sql.Tx, menuName string) (*model.Menu, error) {
-	fmt.Println(menuName)
 	SQL := "SELECT id, category_id, name, default_price, deactivated_at, created_at, created_by, updated_at, updated_by FROM menu WHERE name = ?"
 	rows, err := tx.QueryContext(ctx, SQL, menuName)
 	if err != nil {
 		return nil, err
 	}
 	fmt.Println(rows)
+
+	menu := &model.Menu{}
+	if rows.Next() {
+		rows.Scan(
+			&menu.Id,
+			&menu.CategoryId,
+			&menu.Name,
+			&menu.DefaultPrice,
+			&menu.DeactivatedAt,
+			&menu.CreatedAt,
+			&menu.CreatedBy,
+			&menu.UpdatedAt,
+			&menu.UpdatedBy,
+		)
+		return menu, nil
+	} else {
+		return nil, sql.ErrNoRows
+	}
+}
+
+func (repository *MenuRepositoryImpl) FindMenuById(ctx context.Context, tx *sql.Tx, menuId int) (*model.Menu, error) {
+	SQL := "SELECT id, category_id, name, default_price, deactivated_at, created_at, created_by, updated_at, updated_by FROM menu WHERE id = ?"
+	rows, err := tx.QueryContext(ctx, SQL, menuId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
 	menu := &model.Menu{}
 	if rows.Next() {
