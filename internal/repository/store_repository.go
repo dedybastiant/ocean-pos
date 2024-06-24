7a08dca0d15ef3d2996f0760451d9b6624952dd3
dedybastiant <dedybastian.t@gmail.com>
2024-06-24 08:02:22 +0700

    Add function FindStoreById

--- internal/repository/store_repository.go
+++ internal/repository/store_repository.go
@@ -8,6 +8,7 @@ import (
 
 type StoreRepository interface {
 	InsertStore(ctx context.Context, tx *sql.Tx, store model.Store) (*model.Store, error)
+	FindStoreById(ctx context.Context, tx *sql.Tx, storeId int) (*model.Store, error)
 }
 
 type StoreRepositoryImpl struct{}
@@ -32,4 +33,32 @@ func (repository *StoreRepositoryImpl) InsertStore(ctx context.Context, tx *sql.
 	store.Id = int(id)
 
 	return &store, nil
+}
+
+func (repository *StoreRepositoryImpl) FindStoreById(ctx context.Context, tx *sql.Tx, storeId int) (*model.Store, error) {
+	SQL := "SELECT id, business_id, name, location, description, deactivated_at, created_at, created_by, updated_at, updated_by FROM store WHERE id = ?"
+	rows, err := tx.QueryContext(ctx, SQL, storeId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	store := &model.Store{}
+	if rows.Next() {
+		rows.Scan(
+			&store.Id,
+			&store.BusinessId,
+			&store.Name,
+			&store.Location,
+			&store.Description,
+			&store.DeactivatedAt,
+			&store.CreatedAt,
+			&store.CreatedBy,
+			&store.UpdatedAt,
+			&store.UpdatedBy,
+		)
+		return store, nil
+	} else {
+		return nil, sql.ErrNoRows
+	}
 }
